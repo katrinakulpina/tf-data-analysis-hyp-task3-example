cd8b0161b751dbf3444bb5d4a0200107348b03f9
Ekaterina Kulpina <125750430+katrinakulpina@users.noreply.github.com>
2023-04-13 11:14:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 324047628 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array,
+             y: np.array) -> bool:
+    alpha = 0.07
+    stats, pval = ttest_ind(x, y, equal_var=False, alternative='less')
+    return pval < alpha
