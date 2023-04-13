ad7b54e76aaec3b334fa1d3270a32b1f4a53a549
Ekaterina Kulpina <125750430+katrinakulpina@users.noreply.github.com>
2023-04-13 13:50:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 chat_id = 324047628 # Ваш chat ID, не меняйте название переменной
 
