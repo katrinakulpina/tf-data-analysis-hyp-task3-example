1c6a32644144eadb39fd4c3c7b50869b030a7820
Ekaterina Kulpina <125750430+katrinakulpina@users.noreply.github.com>
2023-04-13 11:14:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,7 @@ from scipy.stats import ttest_ind
 
 chat_id = 324047628 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array,
-             y: np.array) -> bool:
+def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.07
     stats, pval = ttest_ind(x, y, equal_var=False, alternative='less')
     return pval < alpha
